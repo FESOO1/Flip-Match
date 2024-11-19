d80074937dfb64378cc70e968c89ece6ccc84433
FES001 <firdavs7794948@gmail.com>
2024-11-19 21:08:21 +0500

    Working on it.

--- scripts/main.js
+++ scripts/main.js
@@ -3,7 +3,7 @@ const nextButton = document.getElementById('nextButton');
 const startGameButton = document.getElementById('startGameButton');
 const cardsCountArray = ['4','9','16','25'];
 const flipMatchCardsContainerGrid = ['2','3','4','5'];
-const cardsImages = ['../assets/pictures/nature/nature-one.jpg','../assets/pictures/nature/nature-one.jpg','../assets/pictures/nature/nature-two.jpg','../assets/pictures/nature/nature-two.jpg'];
+const cardsImages = ['../assets/pictures/nature/nature-one.jpg','../assets/pictures/nature/nature-one.jpg','../assets/pictures/nature/nature-two.jpg','../assets/pictures/nature/nature-two.jpg', '../assets/pictures/nature/nature-three.jpg','../assets/pictures/nature/nature-three.jpg','../assets/pictures/nature/nature-four.jpg','../assets/pictures/nature/nature-four.jpg','../assets/pictures/nature/nature-five.jpg','../assets/pictures/nature/nature-five.jpg','../assets/pictures/nature/nature-six.jpg','../assets/pictures/nature/nature-six.jpg','../assets/pictures/nature/nature-seven.jpg','../assets/pictures/nature/nature-seven.jpg','../assets/pictures/nature/nature-eight.jpg','../assets/pictures/nature/nature-eight.jpg'];
 let levelCounter = 0;
 
 // START THE GAME
@@ -26,8 +26,13 @@ nextButton.addEventListener('click', () => {
     flipMatchCardsContainer.style.gridTemplateRows = `repeat(${flipMatchCardsContainerGrid[levelCounter]}, 1fr)`;
     flipMatchCardsContainer.innerHTML = null;
     startTheGame();
+
+    // DISABLING THE NEXT BUTTON WHEN LEVEL IS EQUAL TO THE LAST LEVEL OF THE GAME
     if (levelCounter === cardsCountArray.length - 1) {
         nextButton.disabled = true;
+        for (const flipMatchItself of flipMatchCardsContainer.children) {
+            flipMatchItself.style.padding = '2px 3px';
+        };
     };
 });
 
